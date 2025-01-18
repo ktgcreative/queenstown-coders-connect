a7cc8bb486f1a8e8c1bdc0ab50235922df154f44
Kyle G <ktgcreative@gmail.com>
2025-01-18 14:51:05 +1300

    feat: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://qtcc.org'
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://qtcc.co.nz'
 
     return [
         {
